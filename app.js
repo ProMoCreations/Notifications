c13b66c2bc0dda081f93de528dd19984ebac783e
Vladislav Moiseev <v.moiseev94@gmail.com>
2014-05-13 19:06:39 +0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,35 @@
+//requires
+var ws = require('ws');
+var express = require('express');
+
+//global variables
+var clients = {};
+var clientId = 0;
+
+//Static server
+var app = new express();
+app.use(express.static(__dirname + '/public'));
+app.listen(5300);
+console.log('Static server has started');
+
+//WebSocket server
+var webSocketServer = new ws.Server({port: 5301});
+webSocketServer.on('connection', function(ws) {
+	var userId = clientId;
+	clientId++;
+	clients[userId] = ws;
+	console.log("new connection " + userId);
+	
+	ws.on('message', function(data) {
+		console.log('OnMessage: ' + data);
+		for(var key in clients) {
+			clients[key].send(JSON.stringify({id: userId, text: data, nick: 'adminko'}));
+		}
+	});
+
+	ws.on('close', function() {
+		console.log('connection close ' + userId);
+		delete clients[userId];
+	});
+});
+console.log('WebSocket server has started');
---
+++ public/browser.js
@@ -0,0 +1,50 @@
+var myId = 1356;
+
+// показать сообщение в div#subscribe
+showMessage = function(data) {
+	var chatWindow = $('#chatWindow');
+	chatWindow.append($('<div>').text(data.text));
+	if(data.id !== myId) {
+		showNotification(data.nick, data.text);		
+	}
+};
+
+showNotification = function(sender, text) {
+	if (window.webkitNotifications.checkPermission() == 0) {
+		var noty = window.webkitNotifications.createNotification('icon.png', 'Сообщение от ' + sender, text);
+		noty.show();
+		setTimeout(function(){
+			noty.cancel();
+		}, 3000);
+	}
+	else
+	{
+		window.webkitNotifications.requestPermission();
+	}
+};
+
+$(document).ready(function(){
+	$('#sendButton').hide();
+
+	var socket = new WebSocket("ws://192.168.0.111:5301");
+	socket.onopen = function() { 
+		$('#sendButton').show();
+	};
+	
+	socket.onclose = function() { 
+		setTimeout(function(){
+			location.reload();
+		}, 3000);
+	};
+
+	socket.onmessage = function(event) {
+		var incomingMessage = JSON.parse(event.data);
+		showMessage(incomingMessage);
+	};
+	
+	$('#sendButton').click(function(){
+		window.webkitNotifications.requestPermission();
+		var outgoingMessage = document.getElementById('message').value;
+		socket.send(outgoingMessage);
+	});
+});
